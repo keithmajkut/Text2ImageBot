d514c5f28891d1ca8339ca528395308459681109
keithmajkut <kmajkut_us@yahoo.com>
2024-06-18 14:19:45 -0400

    first commit

---
+++ handshake.py
@@ -0,0 +1,10 @@
+import json
+def lambda_handler(event, context):
+    print(f"Received event:\n{event}\nWith context:\n{context}")
+    slack_body = event.get("body")
+    slack_event = json.loads(slack_body)
+    challenge_answer = slack_event.get("challenge")
+    return {
+        'statusCode': 200,
+        'body': challenge_answer
+    }
